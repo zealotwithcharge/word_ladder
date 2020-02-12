35d8fb33f7f1ff97252f303fd4f50820c1f410ea
Mike Izbicki <mike@izbicki.me>
2020-02-12 14:16:38 -0800

    add solution

--- word_ladder.py
+++ word_ladder.py
@@ -32,12 +32,38 @@ def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
     the function returns `None`.
     '''
 
+    if start_word==end_word:
+        return [start_word]
+
+    with open(dictionary_file) as f:
+        words = f.readlines()
+        words = set([ word.strip() for word in words ])
+
+    queue = deque([[start_word]])
+    while len(queue)>0:
+        stack = queue.pop()
+        for word in list(words):
+            if _adjacent(word,stack[-1]):
+                stack_copy = copy.deepcopy(stack)
+                stack_copy.append(word)
+                if word==end_word:
+                    return stack_copy
+                queue.appendleft(stack_copy)
+                words.remove(word)
+
 
 def verify_word_ladder(ladder):
     '''
     Returns True if each entry of the input list is adjacent to its neighbors;
     otherwise returns False.
     '''
+
+    if ladder == []:
+        return False
+    for i in range(len(ladder)-1):
+        if not _adjacent(ladder[i], ladder[i+1]):
+            return False
+    return True
 
 
 def _adjacent(word1, word2):
@@ -50,3 +76,13 @@ def _adjacent(word1, word2):
     >>> _adjacent('stone','money')
     False
     '''
+
+    if len(word1) != len(word2):
+        return False
+
+    num_diffs = 0
+    for c1,c2 in zip(word1,word2):
+        if c1!=c2:
+            num_diffs+=1
+
+    return num_diffs==1
