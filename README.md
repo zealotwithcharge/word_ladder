beaf91e3ab3fe0c28bb624816084b1bd3a310729
Mike Izbicki <mike@izbicki.me>
2021-02-13 14:49:38 -0800

    update test cases

--- README.md
+++ README.md
@@ -1,3 +1,94 @@
-# Word Ladder ![](https://api.travis-ci.com/mikeizbicki/word_ladder.svg?branch=master)
+# Word Ladders with Stacks and Queues
+[![](https://github.com/mikeizbicki/word_ladder/workflows/tests/badge.svg)](https://github.com/mikeizbicki/word_ladder/actions?query=workflow%3Atests)
 
-This is hw4 for CMC's [CS46: data structures](https://github.com/mikeizbicki/cmc-csci046) course.
+You will implement a solution to Lewis Carroll's [word ladder game](https://en.wikipedia.org/wiki/Word_ladder).
+
+**Learning Objectives:**
+
+1. implement a complex algorithm involving both queues and stacks
+1. understand python memory management
+1. practice translating pseudocode into python
+
+**Relation to technical interviews:**
+
+[Leetcode](https://leetcode.com/problemset/all/) is a common method used for technical interviews.
+The world ladder problem is an example of a problem that leetcode puts in their hardest-to-solve category (see [the problem description on leetcode.com](https://leetcode.com/problems/word-ladder/)).
+What makes these problems "hard" is figuring out the right pseudocode.
+Translating from pseudocode into working code is considered "easy".
+
+In this assignment, we will practice the translation of pseudocode into working python code.
+If you want to learn more about how to create the pseudocode in the first place,
+then you should take CSCI148: Graph Algorithms with Prof Sarah Cannon.
+
+## Background
+
+A word ladder is a list of words where each word differs by only a single letter from the previous word.
+For example, we can convert a `stone` into `money` using the following ladder:
+
+```
+stone
+atone
+alone
+clone
+coons
+conns
+cones
+coney
+money
+```
+
+In this assignment, you will implement a function `word_ladder` that generates these word ladders.
+There are many possible algorithms to generate word ladders,
+but a simple one uses a combination of stacks and queues as shown in the following pseudocode.
+
+```
+Create a stack
+Push the start word onto the stack
+Create a queue
+Enqueue the stack onto the queue
+
+While the queue is not empty
+    Dequeue a stack from the queue
+    For each word in the dictionary
+        If the word is adjacent to the top of the stack
+            If this word is the end word
+                You are done!
+                The front stack plus this word is your word ladder.
+            Make a copy of the stack
+            Push the found word onto the copy
+            Enqueue the copy
+            Delete word from the dictionary
+```
+
+**HINTs:**
+
+1. This pseudocode is intentionally vague,
+   and I encourage you to ask clarifying questions.
+   The ability to ask good questions is one of the keys skills being tested in technical interviews.
+
+1. One of the main differences between pseudocode and real code is that pseudocode doesn't have to deal with memory management, but real code does.
+   The vast majority of bugs that students get with this assignment is due to memory management issues.
+
+1. The test cases take a long time to run, and you will not be efficient if you are re-running them all the time.
+   Instead, use the `--last-failed` flag in pytest to skip the tests that you have previously passed and only run the failing tests.
+
+## Tasks
+
+Complete the following tasks:
+
+1. Fork the this repo and enable github actions
+1. Update the `README.md` file so that the travis button points to your repo
+1. Implement the `word_ladder`, `verify_word_ladder`, and `_adjacent` functions so that all test cases in `tests/test_main.py` pass
+
+Your grade will be the percentage of test cases that successfully pass in travis.
+
+## Submission
+
+Submit the link to your forked repository on sakai.
+
+## Collaboration Policy
+
+**You are not allowed to look at another student's github repo.**
+
+All other forms of collaboration with other students are encouraged.
+You may use any other online resources you like to complete this assignment.
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,11 @@
 from word_ladder import _adjacent,verify_word_ladder,word_ladder
-
+import random
 
 def test__word_ladder_1():
     assert not _adjacent('stone','money')
+
+def test__word_ladder_1b():
+    assert not _adjacent('money','stone')
 
 def test__word_ladder_2():
     assert not _adjacent('stone','stone1')
@@ -13,9 +16,34 @@ def test__word_ladder_3():
 def test__word_ladder_4():
     assert _adjacent('stone','stony')
 
+def test__word_ladder_4v():
+    assert _adjacent('stony','stone')
+
 def test__word_ladder_5():
     assert _adjacent('stone','shone')
 
+def test__word_ladder_5b():
+    assert _adjacent('shone','stone')
+
+def test__word_ladder_6():
+    assert not _adjacent('shone','shone')
+
+def test__word_ladder_7():
+    assert not _adjacent('','shone')
+
+def test__word_ladder_7b():
+    assert not _adjacent('shone','')
+
+def test__word_ladder_fuzz():
+    with open('words5.dict') as f:
+        words = f.readlines()
+        words = list(set([ word.strip() for word in words ]))
+    for i in range(1000000):
+        word1 = random.choice(words)
+        word2 = random.choice(words)
+        res1 = _adjacent(word1, word2)
+        res2 = _adjacent(word2, word1)
+        assert res1 == res2 or res1 is res2
 
 def test__verify_word_ladder_1():
     assert not verify_word_ladder([])
@@ -32,18 +60,45 @@ def test__verify_word_ladder_4():
 def test__verify_word_ladder_5():
     assert verify_word_ladder(['stone', 'shone', 'phone', 'phony', 'peony', 'penny', 'benny', 'bonny', 'boney', 'money'])
 
+def test__verify_word_ladder_5b():
+    assert verify_word_ladder(list(reversed(['stone', 'shone', 'phone', 'phony', 'peony', 'penny', 'benny', 'bonny', 'boney', 'money'])))
+
 def test__verify_word_ladder_6():
     assert not verify_word_ladder(['stone', 'phone', 'phony', 'peony', 'penny', 'benny', 'bonny', 'boney', 'money'])
+
+def test__verify_word_ladder_6b():
+    assert not verify_word_ladder(list(reversed(['stone', 'phone', 'phony', 'peony', 'penny', 'benny', 'bonny', 'boney', 'money'])))
 
 def test__verify_word_ladder_7():
     assert not verify_word_ladder(['stone', 'shone', 'phone', 'phony', 'peony', 'benny', 'bonny', 'boney', 'money'])
 
+def test__verify_word_ladder_7b():
+    assert not verify_word_ladder(list(reversed(['stone', 'shone', 'phone', 'phony', 'peony', 'benny', 'bonny', 'boney', 'money'])))
+
 def test__verify_word_ladder_8():
     assert not verify_word_ladder(['stone', 'shone', 'phone', 'phony', 'peony', 'penny', 'benny', 'bonny', 'money'])
+
+def test__verify_word_ladder_8b():
+    assert not verify_word_ladder(list(reversed(['stone', 'shone', 'phone', 'phony', 'peony', 'penny', 'benny', 'bonny', 'money'])))
 
 # tests for memory management correctness
 def test__verify_word_ladder_9():
     assert verify_word_ladder(['chins', 'chine'])
+
+def test__verify_word_ladder_9b():
+    assert verify_word_ladder(['chine', 'chins'])
+
+def test__verify_word_ladder_10():
+    assert verify_word_ladder(['chins', 'chink'])
+
+def test__verify_word_ladder_10b():
+    assert verify_word_ladder(['chink', 'chins'])
+
+def test__verify_word_ladder_11():
+    assert verify_word_ladder(['chine', 'chink'])
+
+def test__verify_word_ladder_11b():
+    assert verify_word_ladder(['chink', 'chine'])
 
 
 def test__word_ladder_1():
@@ -58,8 +113,16 @@ def test__word_ladder_3():
     ladder = word_ladder('stone','shone')
     assert verify_word_ladder(ladder) and len(ladder)==2
 
+def test__word_ladder_3b():
+    ladder = word_ladder('shone','stone')
+    assert verify_word_ladder(ladder) and len(ladder)==2
+
 def test__word_ladder_4():
     ladder = word_ladder('dears','fears')
+    assert verify_word_ladder(ladder) and len(ladder)==2
+
+def test__word_ladder_4b():
+    ladder = word_ladder('fears','dears')
     assert verify_word_ladder(ladder) and len(ladder)==2
 
 def test__word_ladder_5():
@@ -105,8 +168,25 @@ def test__word_ladder_14():
 def test__word_ladder_15():
     assert word_ladder('atlas','zebra') is None
 
+def test__word_ladder_15b():
+    assert word_ladder('zebra','atlas') is None
+
 def test__word_ladder_16():
     assert word_ladder('aloof','money') is None
 
+def test__word_ladder_16b():
+    assert word_ladder('money','aloof') is None
+
 def test__word_ladder_17():
     assert word_ladder('data','structures') is None
+
+def test__word_ladder_fuzz():
+    with open('words5.dict') as f:
+        words = f.readlines()
+        words = list(set([ word.strip() for word in words ]))
+    for i in range(20):
+        word1 = random.choice(words)
+        word2 = random.choice(words)
+        res1 = word_ladder(word1, word2)
+        res2 = list(reversed(word_ladder(word2, word1)))
+        assert res1 == res2 or res1 is res2
