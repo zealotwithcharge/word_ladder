3be15fcf9659e716261dfc451d58ea0e512d7281
zealotwithcharge <chwang22@cmc.edu>
2022-02-23 14:37:59 -0800

    finished

--- word_ladder.py
+++ word_ladder.py
@@ -2,7 +2,7 @@
 
 from collections import deque
 import copy
-
+import pprint
 def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
     '''
     Returns a list satisfying the following properties:
@@ -30,29 +30,45 @@ def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
     Whenever it is impossible to generate a word ladder between the two words,
     the function returns `None`.
     '''
-    stack = [start_word]
-    q = deque([stack])
-    print(q) 
+    stack = []
+    stack.append(start_word)
+    q = deque([])
+    q.append(stack)
+    answers = []
+    if start_word == end_word:
+        return [start_word]
     with open(dictionary_file,'r',encoding = 'utf-8') as dict_file:
         dictionary = dict_file.read()
         dictionary = dictionary.split('\n')
         while len(q) != 0:
             working_stack = q.popleft()
-            print(working_stack)
-            for word in dictionary:
+            dict_copy = copy.deepcopy(dictionary) 
+            for word in dict_copy:
                 if _adjacent(word,working_stack[-1]):
                     if word == end_word:
-                        print(working_stack) 
                         if end_word not in working_stack:
                             working_stack.append(end_word)
-                        return working_stack
+                        #answers.append(working_stack)
+                        #continue
+                            #pprint.pprint(q)
+                            #for i in range(len(q)):
+                            #    temp = q.popleft()
+                            #    if temp[7] == 'chile' or temp[7] == 'chili' or temp[7] == 'chill':
+                            #        print(temp) 
+                            return working_stack
                     stack_copy = copy.deepcopy(working_stack)
                     stack_copy.append(word)
                     q += deque([stack_copy])
                     dictionary.remove(word)
-            print('q after dict is ',q)
-    return
-
+    return 
+    #if answers == []:
+    #    return
+    #else:
+    #    smallest = 0
+    #    for i,ladder in enumerate(answers):
+    #        if len(answers[smallest]) > len(answers[i]):
+    #           smallest = i
+    #    return answers[smallest]
 def verify_word_ladder(ladder):
     '''
     Returns True if each entry of the input list is adjacent to its neighbors;
@@ -68,6 +84,8 @@ def verify_word_ladder(ladder):
         if i != len(ladder)-1:
             if not _adjacent(ladder[i],ladder[i+1]):
                 return False
+    if len(ladder) == 0:
+        return False
     return True
 
 def _adjacent(word1, word2):
@@ -91,3 +109,12 @@ def _adjacent(word1, word2):
         if word1[i] != word2[i]:
             counter += 1
     return counter == 1
+def check_links(goal):
+    with open('words5.dict','r',encoding = 'utf-8') as dict_file:
+        dictionary = dict_file.read()
+        dictionary = dictionary.split('\n')
+        answers = []
+        for word in dictionary:
+            if _adjacent(word,goal):
+                answers.append(word)
+        print(answers)
